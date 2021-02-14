deb873966cbc7cf0138263f4bc097fab98448545
georgy-kirilov <georgynk17@gmail.com>
2021-02-14 21:01:17 +0200

    Hristov problem 5

---
+++ Recursion/src/hristov/feb_15_2021/P05.java
@@ -0,0 +1,43 @@
+package hristov.feb_15_2021;
+
+import actions.BigAction;
+
+public class P05
+{
+	public static void main(String[] args)
+	{
+		printTriangleOfNumbers(3, 9);
+		printTriangleOfNumbers(1, 5);
+		printTriangleOfNumbers(10, 15);
+	}
+	
+	static void printTriangleOfNumbers(int cols, int rows)
+	{	
+		BigAction<Integer, Integer, Integer> action = new BigAction<Integer, Integer, Integer>()
+		{	
+			@Override
+			public void invoke(Integer cols, Integer rows, Integer col)
+			{
+				if (col >= cols + 1)
+				{
+					col = 1;
+					cols++;
+					System.out.println();
+				}
+				
+				if (cols >= rows + 1)
+				{
+					System.out.println();
+					return;
+				}
+				
+				System.out.print(col);
+				col++;
+				
+				this.invoke(cols, rows, col);
+			}
+		};
+		
+		action.invoke(cols, rows, 1);
+	}
+}
